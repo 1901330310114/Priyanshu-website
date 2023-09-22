514f874e2b37653c0b868de093501af5cb4b1ec8
1901330310114 <99010517+1901330310114@users.noreply.github.com>
2023-09-22 08:32:25 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app=Flask(__name__)  #object of flask class
 #name -> refers to how script is invoked. will give main if invoked from .py file
 
 @app.route("/")  #route gives path, @ is a decorator. / is home page.
 def hello_world():
-  return "Hello world"
+  return render_template('home.html')
 
 if __name__=="__main__":# when script run throgh python
   app.run(host='0.0.0.0', debug=True)  #0.0.0.0 for running locally and debug if we change
